95ef6a57104339c606fe215d57c0a2545dd510e1
wfx <fungtion.wong@gmail.com>
2018-03-17 09:28:27 +0800

    add diffloss and simse loss

--- functions.py
+++ functions.py
@@ -1,4 +1,6 @@
 from torch.autograd import Function
+import torch.nn as nn
+import torch
 
 
 class ReverseLayerF(Function):
@@ -14,5 +16,41 @@ class ReverseLayerF(Function):
         output = grad_output.neg() * ctx.alpha
 
         return output, None
+
+
+class SIMSE(nn.Module):
+
+    def __init__(self):
+        super(SIMSE, self).__init__()
+
+    def forward(self, real, pred):
+        diffs = real - pred
+        num_present = torch.numel(diffs)
+        simse = torch.sum(diffs).__pow__(2).sum() / num_present
+
+        return simse
+
+
+class DiffLoss(nn.Module):
+
+    def __init__(self):
+        super(DiffLoss, self).__init__()
+
+    def forward(self, input1, input2):
+
+        batch_size = input1.size(0)
+        input1 = input1.view(batch_size, -1)
+        input2 = input2.view(batch_size, -1)
+
+        input1_l2_norm = torch.norm(input1, p=2, dim=1, keepdim=True).detach()
+        input1_l2 = input1.div(input1_l2_norm.expand_as(input1))
+
+        input2_l2_norm = torch.norm(input2, p=2, dim=1, keepdim=True).detach()
+        input2_l2 = input2.div(input2_l2_norm.expand_as(input2))
+
+        diff_loss = torch.mean(input1_l2.t().mm(input2_l2).pow(2))
+
+        return diff_loss
+
 
 
