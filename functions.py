ef22e0e077858552dfabff05c2f1ae19b1070a2b
wfx <fungtion.wong@gmail.com>
2018-03-23 21:11:43 +0800

    first feasible model

--- functions.py
+++ functions.py
@@ -6,16 +6,28 @@ import torch
 class ReverseLayerF(Function):
 
     @staticmethod
-    def forward(ctx, x, alpha):
-        ctx.alpha = alpha
+    def forward(ctx, x, p):
+        ctx.p = p
 
         return x.view_as(x)
 
     @staticmethod
     def backward(ctx, grad_output):
-        output = grad_output.neg() * ctx.alpha
+        output = grad_output.neg() * ctx.p
 
         return output, None
+
+
+class MSE(nn.Module):
+    def __init__(self):
+        super(MSE, self).__init__()
+
+    def forward(self, pred, real):
+        diffs = torch.add(real, -pred)
+        n = torch.numel(diffs.data)
+        mse = torch.sum(diffs.pow(2)) / n
+
+        return mse
 
 
 class SIMSE(nn.Module):
@@ -23,10 +35,10 @@ class SIMSE(nn.Module):
     def __init__(self):
         super(SIMSE, self).__init__()
 
-    def forward(self, real, pred):
-        diffs = real - pred
-        num_present = torch.numel(diffs)
-        simse = torch.sum(diffs).__pow__(2).sum() / num_present
+    def forward(self, pred, real):
+        diffs = torch.add(real, - pred)
+        n = torch.numel(diffs.data)
+        simse = torch.sum(diffs).pow(2) / (n ** 2)
 
         return simse
 
@@ -43,12 +55,12 @@ class DiffLoss(nn.Module):
         input2 = input2.view(batch_size, -1)
 
         input1_l2_norm = torch.norm(input1, p=2, dim=1, keepdim=True).detach()
-        input1_l2 = input1.div(input1_l2_norm.expand_as(input1))
+        input1_l2 = input1.div(input1_l2_norm.expand_as(input1) + 1e-6)
 
         input2_l2_norm = torch.norm(input2, p=2, dim=1, keepdim=True).detach()
-        input2_l2 = input2.div(input2_l2_norm.expand_as(input2))
+        input2_l2 = input2.div(input2_l2_norm.expand_as(input2) + 1e-6)
 
-        diff_loss = torch.mean(input1_l2.t().mm(input2_l2).pow(2))
+        diff_loss = torch.mean((input1_l2.mm(input2_l2.t()).pow(2)))
 
         return diff_loss
 
